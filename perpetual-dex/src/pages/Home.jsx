366bcb25e045b7821888cf0d067078ac4cb72fcd
DivyanshGoel20 <divyanshgoel17@gmail.com>
2024-12-04 11:39:08 +0530

    add: Header for Home and Trade page

--- perpetual-dex/src/pages/Home.jsx
+++ perpetual-dex/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "../components/HomeNavbar";
 import { Link } from "react-router-dom";
 import "../index.css";
 
 const Home = () => {
   return (
     <div style={styles.container}>
+      <Navbar />
       <h1 style={styles.heading}>
         Trade <span style={styles.highlight}>Perpetuals</span>
       </h1>
